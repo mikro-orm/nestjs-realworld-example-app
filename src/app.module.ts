79ef37615881c6de5409fecd3f412119b0995f84
Martin Adámek <banan23@gmail.com>
2025-01-28 20:01:02 +0100

    chore: update nest adapter to v6.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { MiddlewareConsumer, Module, NestModule, OnModuleInit } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { MikroORM } from '@mikro-orm/mysql';
-import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { AppController } from './app.controller';
 import { ArticleModule } from './article/article.module';
@@ -21,21 +21,12 @@ import { UserModule } from './user/user.module';
   ],
   providers: [],
 })
-export class AppModule implements NestModule, OnModuleInit {
+export class AppModule implements OnModuleInit {
 
   constructor(private readonly orm: MikroORM) {}
 
   async onModuleInit(): Promise<void> {
     await this.orm.getMigrator().up();
-  }
-
-  // for some reason the auth middlewares in profile and article modules are fired before the request context one,
-  // so they would fail to access contextual EM. by registering the middleware directly in AppModule, we can get
-  // around this issue
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(MikroOrmMiddleware)
-      .forRoutes('*');
   }
 
 }
